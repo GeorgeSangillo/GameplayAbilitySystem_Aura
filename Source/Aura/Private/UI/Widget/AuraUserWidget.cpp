d507af214d55980c5d20c3d5cba3575981dae564
George Sangillo <46853261+GeorgeSangillo@users.noreply.github.com>
2025-04-06 13:42:25 -0500

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright George R Sangillo
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
