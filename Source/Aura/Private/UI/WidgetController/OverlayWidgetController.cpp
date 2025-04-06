435e48688af50271b5971402ad09ba23d05af74b
George Sangillo <46853261+GeorgeSangillo@users.noreply.github.com>
2025-04-06 18:51:06 -0500

    Listening for Attribute Changes

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -12,3 +12,24 @@ void UOverlayWidgetController::BroadcastInitialValues()
 	OnHealthUpdated.Broadcast(AuraAttributeSet->GetHealth());
 	OnMaxHealthUpdated.Broadcast(AuraAttributeSet->GetMaxHealth());
 }
+
+void UOverlayWidgetController::BindCallbacks()
+{
+	const UAuraAttributeSet* AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
+		AuraAttributeSet->GetHealthAttribute()).AddUObject(this, &UOverlayWidgetController::HealthUpdated);
+
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
+	AuraAttributeSet->GetMaxHealthAttribute()).AddUObject(this, &UOverlayWidgetController::MaxHealthUpdated);
+}
+
+void UOverlayWidgetController::HealthUpdated(const FOnAttributeChangeData& Data) const
+{
+	OnHealthUpdated.Broadcast(Data.NewValue);
+}
+
+void UOverlayWidgetController::MaxHealthUpdated(const FOnAttributeChangeData& Data) const
+{
+	OnMaxHealthUpdated.Broadcast(Data.NewValue);
+}
--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -6,6 +6,7 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "OverlayWidgetController.generated.h"
 
+struct FOnAttributeChangeData;
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthUpdatedSignature, float, NewHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthUpdatedSignature, float, NewMaxHealth);
 
@@ -18,10 +19,15 @@ class AURA_API UOverlayWidgetController : public UAuraWidgetController
 	GENERATED_BODY()
 public:
 	virtual void BroadcastInitialValues() override;
+	virtual void BindCallbacks() override;
 
 	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
 	FOnHealthUpdatedSignature OnHealthUpdated;
 
 	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
 	FOnHealthUpdatedSignature OnMaxHealthUpdated;
+
+protected:
+	void HealthUpdated(const FOnAttributeChangeData& Data) const;
+	void MaxHealthUpdated(const FOnAttributeChangeData& Data) const;
 };
